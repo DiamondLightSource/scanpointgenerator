52997757fdfb355474ec1380e3940ac6d2421b82
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-05-17 11:54:36 +0100

    Add double nesting test

--- tests/test_nestedgenerator.py
+++ tests/test_nestedgenerator.py
@@ -32,5 +32,23 @@ class NestedGeneratorTest(ScanPointGeneratorTest):
                 y=ypositions[i], x=xupper[i]))
             self.assertEqual(p.indexes, [yindexes[i], xindexes[i]])
 
+    def test_double_nest(self):
+        self.z = LineGenerator("z", "mm", 1.0, 1.0, 2)
+        self.g2 = NestedGenerator(self.z, self.g)
+
+        zpositions = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+                      2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+        ypositions = [2.0, 2.0, 2.0, 2.1, 2.1, 2.1,
+                      2.0, 2.0, 2.0, 2.1, 2.1, 2.1]
+        xpositions = [1.0, 1.1, 1.2, 1.2, 1.1, 1.0,
+                      1.0, 1.1, 1.2, 1.2, 1.1, 1.0]
+        zindexes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+        yindexes = [0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]
+        xindexes = [0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0]
+        for i, p in enumerate(self.g2.iterator()):
+            self.assertEqual(p.positions, dict(
+                z=zpositions[i], y=ypositions[i], x=xpositions[i]))
+            self.assertEqual(p.indexes, [zindexes[i], yindexes[i], xindexes[i]])
+
 if __name__ == "__main__":
     unittest.main()
