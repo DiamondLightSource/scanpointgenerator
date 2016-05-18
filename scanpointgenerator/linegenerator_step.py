6a8068942d985f0c125749d5f58ad7cb9cd189be
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-05-18 15:22:29 +0100

    Add extra point to include start

--- scanpointgenerator/linegenerator_step.py
+++ scanpointgenerator/linegenerator_step.py
@@ -1,11 +1,10 @@
 from linegenerator import LineGenerator
-import math as m
 
 
 class StepLineGenerator(LineGenerator):
 
     def __init__(self, name, units, start, end, step):
 
-        num = int(m.floor((end - start)/step))
+        num = int((end - start)/step) + 1
 
         super(StepLineGenerator, self).__init__(name, units, start, step, num)
