f4995e99649bf29126673f2a1a440151898416f3
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-05-18 15:21:32 +0100

    Add half step offset

--- scanpointgenerator/linegenerator_npoint.py
+++ scanpointgenerator/linegenerator_npoint.py
@@ -6,9 +6,6 @@ class NPointLineGenerator(LineGenerator):
     def __init__(self, name, units, start, end, num):
 
         step = (end - start)/num
-        #
-        # step = (end - start)/(num + 1)
-        # start += step/2
-        # end -= step/2
+        start += step/2
 
         super(NPointLineGenerator, self).__init__(name, units, start, step, num)
