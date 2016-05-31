0ede1ac23426ab6eb9a8a05332bb933a876e92e4
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-05-31 13:30:49 +0100

    Fix spiral generator to fill circle

--- scanpointgenerator/spiralgenerator.py
+++ scanpointgenerator/spiralgenerator.py
@@ -5,13 +5,13 @@ import math as m
 
 class SpiralGenerator(ScanPointGenerator):
 
-    def __init__(self, names, units, centre, radius):
+    def __init__(self, names, units, centre, radius, scale=1.0):
         self.name = names
         self.units = units
         self.centre = centre
         self.radius = radius
         self.alpha = m.sqrt(4 * m.pi)  # Theta scale factor
-        self.beta = radius / (2 * m.pi)  # Radius scale factor
+        self.beta = scale / (2 * m.pi)  # Radius scale factor
 
         self.position_units = {names[0]: units, names[1]: units}
         self.index_dims = [self._end_point(self.radius) - 1]
@@ -19,7 +19,7 @@ class SpiralGenerator(ScanPointGenerator):
 
     def _calc(self, i):
         """Calculate the coordinate for a given index"""
-        theta = m.sqrt(i)
+        theta = self.alpha * m.sqrt(i)
         radius = self.beta * theta
         x = self.centre[0] + radius * m.sin(theta)
         y = self.centre[1] + radius * m.cos(theta)
@@ -28,7 +28,7 @@ class SpiralGenerator(ScanPointGenerator):
 
     def _end_point(self, radius):
         """Calculate the index of the final point contained by circle"""
-        return int((radius / (self.alpha + self.beta)) ** 2)
+        return int((radius / (self.alpha * self.beta)) ** 2)
 
     def iterator(self):
         for i in range(1, self._end_point(self.radius)):
