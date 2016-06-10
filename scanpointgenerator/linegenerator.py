393242c42ae24b3bac35d669187fd451262cf87d
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-10 13:56:29 +0100

    Make LineGenerator generic for N dimensional space

--- scanpointgenerator/linegenerator.py
+++ scanpointgenerator/linegenerator.py
@@ -2,39 +2,62 @@ from scanpointgenerator import ScanPointGenerator
 from point import Point
 
 
+def to_list(value):
+        if isinstance(value, list):
+            return value
+        else:
+            return [value]
+
+
 class LineGenerator(ScanPointGenerator):
-    """Generate equally spaced scan points in one dimension"""
+    """Generate equally spaced scan points in N dimensions"""
 
     def __init__(self, name, units, start, stop, num):
         """Initialise the generator
 
         Args:
-            name (str): The scannable name. E.g. "x"
+            name (str/list(str)): The scannable name(s) E.g. "x" or ["x", "y"]
             units (str): The scannable units. E.g. "mm"
-            start (float): The first position to be generated. E.g. 1.0
-            stop (float): The first position to be generated. E.g. 9.0
+            start (float/list(float)): The first position to be generated
+            E.g. 1.0 or [1.0, 2.0]
+            stop (float or list(float)): The first position to be generated. E.g. 9.0
             num (int): The number of points to generate. E.g. 5
         """
 
-        self.name = name
-        self.start = start
-        self.stop = stop
+        self.name = to_list(name)
+        self.start = to_list(start)
+        self.stop = to_list(stop)
+
+        if len(self.name) != len(self.start) or \
+           len(self.name) != len(self.stop):
+            raise ValueError(
+                "Dimensions of name, start and stop do not match")
+
         self.num = num
-        self.step = abs(self.stop - self.start)/(self.num - 1)
+        self.num_axes = len(name)
 
-        self.position_units = {name: units}
+        self.step = []
+        for axis in range(len(self.start)):
+            self.step.append(
+                abs(self.stop[axis] - self.start[axis])/(self.num - 1))
+
+        self.position_units = {}
+        for dimension in self.name:
+            self.position_units[dimension] = units
         self.index_dims = [self.num]
-        self.index_names = [name]
+        self.index_names = self.name
 
-    def _calc(self, i):
+    def _calc(self, i, axis):
         """Calculate the position for a given index"""
-        return self.start + i * self.step
+        return self.start[axis] + i * self.step[axis]
 
     def iterator(self):
         for i in xrange(self.num):
             point = Point()
-            point.positions[self.name] = self._calc(i)
-            point.lower[self.name] = self._calc(i - 0.5)
-            point.upper[self.name] = self._calc(i + 0.5)
+
+            for axis in range(self.num_axes):
+                point.positions[self.name[axis]] = self._calc(i, axis)
+                point.lower[self.name[axis]] = self._calc(i - 0.5, axis)
+                point.upper[self.name[axis]] = self._calc(i + 0.5, axis)
             point.indexes = [i]
             yield point
--- tests/test_linegenerator.py
+++ tests/test_linegenerator.py
@@ -26,6 +26,31 @@ class LineGeneratorTest(ScanPointGeneratorTest):
             self.assertEqual(p.indexes, [indexes[i]])
         self.assertEqual(i, 4)
 
+
+class LineGenerator2DTest(ScanPointGeneratorTest):
+
+    def setUp(self):
+        self.g = LineGenerator(["x", "y"], "mm", [1.0, 2.0], [5.0, 10.0], 5)
+
+    def test_init(self):
+        self.assertEqual(self.g.position_units, dict(x="mm", y="mm"))
+        self.assertEqual(self.g.index_dims, [5])
+        self.assertEqual(self.g.index_names, ["x", "y"])
+
+    def test_iterator(self):
+        x_positions = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y_positions = [2.0, 4.0, 6.0, 8.0, 10.0]
+        lower = [0.5, 1.5, 2.5, 3.5, 4.5]
+        upper = [1.5, 2.5, 3.5, 4.5, 5.5]
+        indexes = [0, 1, 2, 3, 4]
+        for i, p in enumerate(self.g.iterator()):
+            self.assertEqual(p.positions, dict(x=x_positions[i],
+                                               y=y_positions[i]))
+            self.assertEqual(p.lower["x"], lower[i])
+            self.assertEqual(p.upper["x"], upper[i])
+            self.assertEqual(p.indexes, [indexes[i]])
+        self.assertEqual(i, 4)
+
 if __name__ == "__main__":
     unittest.main()
 
