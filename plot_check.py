506994784c6078bc30fe0a7c5d14f22face2a3ae
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-13 10:02:04 +0100

    Add some more plots

--- plot_check.py
+++ plot_check.py
@@ -1,11 +1,12 @@
-from scanpointgenerator import LineGenerator, NestedGenerator, plot_generator
+from scanpointgenerator import LineGenerator, NestedGenerator
 from scanpointgenerator.rectangular_roi import RectangularROI
 from scanpointgenerator.circular_roi import CircularROI
-from scanpointgenerator.rastergenerator import RasterGenerator
 from scanpointgenerator.gridgenerator import GridGenerator
 from scanpointgenerator.spiralgenerator import SpiralGenerator
 from scanpointgenerator.lissajousgenerator import LissajousGenerator
+from scanpointgenerator.randomoffsetgenerator import RandomOffsetGenerator
 from scanpointgenerator.factory import Factory
+from plotgenerator2 import plot_generator
 
 from pkg_resources import require
 require('matplotlib')
@@ -22,19 +23,6 @@ def test_check():
     rectangle = RectangularROI([2.0, 2.5], 3.0, 2.0)
 
     plot_generator(gen, rectangle)
-
-
-def raster_circle_check():
-
-    bounding_box = dict(centre=[2.0, 1.0], width=4.0, height=2.0)
-    inner_scan = dict(name='x', units='mm', step=2.0)
-    outer_scan = dict(name='y', units='mm', step=1.0)
-
-    gen = RasterGenerator(bounding_box, inner_scan, outer_scan, alternate_direction=True)
-
-    circle = CircularROI([2.0, 1.0], 2.0)
-
-    plot_generator(gen, circle)
 
 
 def grid_circle_check():
@@ -55,7 +43,7 @@ def grid_circle_check():
 
 def spiral_check():
 
-    gen = SpiralGenerator(['x', 'y'], "mm", [0.0, 0.0], 5.0)
+    gen = SpiralGenerator(['x', 'y'], "mm", [0.0, 0.0], 10.0)
     plot_generator(gen)
 
 
@@ -72,7 +60,26 @@ def spiral_rectangle_check():
 def lissajous_check():
 
     bounding_box = dict(centre=[0.0, 0.0], width=1.0, height=1.0)
-    gen = LissajousGenerator(['x', 'y'], "mm", bounding_box, 8, [1, 2])
+    gen = LissajousGenerator(['x', 'y'], "mm", bounding_box, 2)
+
+    plot_generator(gen)
+
+
+def line_2d_check():
+
+    gen = LineGenerator(["x", "y"], "mm", [1.0, 2.0], [5.0, 10.0], 5)
+
+    plot_generator(gen)
+
+
+def random_offset_check():
+
+    bounding_box = dict(centre=[2.0, 1.0], width=4.0, height=2.0)
+    inner_scan = dict(name='x', units='mm', num=4)
+    outer_scan = dict(name='y', units='mm', num=3)
+
+    line = GridGenerator(bounding_box, inner_scan, outer_scan, alternate_direction=True)
+    gen = RandomOffsetGenerator(line, 2, dict(x=0.25, y=0.25))
 
     plot_generator(gen)
 
@@ -80,6 +87,8 @@ def lissajous_check():
 # test_check()
 # raster_circle_check()
 # grid_circle_check()
-# spiral_check()
+spiral_check()
 # spiral_rectangle_check()
-lissajous_check()
+# lissajous_check()
+# line_2d_check()
+# random_offset_check()
