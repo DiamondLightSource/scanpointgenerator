81df73088824dc6a00f7219a4dabbdb3b3043d46
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-15 08:44:23 +0100

    Add serialisation to temporary CircularROI

--- scanpointgenerator/circular_roi.py
+++ scanpointgenerator/circular_roi.py
@@ -18,3 +18,30 @@ class CircularROI(ROI):
             return False
         else:
             return True
+
+    def to_dict(self):
+        """Convert object attributes into a dictionary"""
+
+        d = super(CircularROI, self).to_dict()
+        d['type'] = "CircularROI"
+        d['radius'] = self.radius
+
+        return d
+
+    @classmethod
+    def from_dict(cls, d):
+        """
+        Create a CircularROI instance from a serialised dictionary
+
+        Args:
+            d(dict): Dictionary of attributes
+
+        Returns:
+            CircularROI: New CircularROI instance
+        """
+
+        name = d['name']
+        centre = d['centre']
+        radius = d['radius']
+
+        return cls(name, centre, radius)
