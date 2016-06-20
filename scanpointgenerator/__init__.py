4e50ffff6c9f80bef7bb71ac0f57ed6993b89925
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-06-20 14:56:09 +0100

    Remove maskedgenerator import

--- scanpointgenerator/__init__.py
+++ scanpointgenerator/__init__.py
@@ -4,7 +4,6 @@ from arraygenerator import ArrayGenerator  # noqa
 from compoundgenerator import CompoundGenerator  # noqa
 from linegenerator import LineGenerator  # noqa
 from lissajousgenerator import LissajousGenerator  # noqa
-from maskedgenerator import MaskedGenerator  # noqa
 from randomoffsetgenerator import RandomOffsetGenerator  # noqa
 from spiralgenerator import SpiralGenerator  # noqa
 from plotgenerator import plot_generator  # noqa
