10f6e83b2b5529fde125f7a0d2d007ea0d347bcd
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-08-16 12:12:27 +0100

    Refactor update script to extract release version

--- update_source_only_repo.sh
+++ update_source_only_repo.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "Must provide a release version"
-    exit 1
-else
-    VER="$1"
-fi
+VER=$(python -c "from scanpointgenerator.version import __version__; print(__version__)")
+
+echo "" &&
+echo "Updating daq-eclipse in GDA. Please wait..." &&
+echo "" &&
 
 git clone git@github.com:dls-controls/scanpointgenerator.git /tmp/scanpointgenerator &&
 git clone git@github.com:DiamondLightSource/daq-eclipse.git /tmp/daq-eclipse &&
 cd /tmp/daq-eclipse &&
-git checkout -b spg-release-$(VER) &&
+git checkout -b spg-release-$VER &&
+git rm -r org.eclipse.scanning.points/scripts/scanpointgenerator &&
 cp -r ../scanpointgenerator/scanpointgenerator/ org.eclipse.scanning.points/scripts/scanpointgenerator &&
 git add -A &&
-git commit -m "Update scanpointgenerator to version $(VER)"
-git push origin release-$(VER)
+git commit -m "Update scanpointgenerator to version $VER" &&
+git push origin spg-release-$VER
+
+echo "" &&
+echo "\nUpdate successful"
 
 rm -rf /tmp/scanpointgenerator
 rm -rf /tmp/daq-eclipse
