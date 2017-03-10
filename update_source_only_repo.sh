b224461364d48d1e5a39df5d01f029f28ebe989b
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-03-10 09:11:33 +0000

    Update publish script to publish to new eclipse/scanning repo

--- update_source_only_repo.sh
+++ update_source_only_repo.sh
@@ -3,21 +3,21 @@
 VER=$(python -c "from scanpointgenerator.version import __version__; print(__version__)")
 
 echo "" &&
-echo "Updating daq-eclipse in GDA. Please wait..." &&
+echo "Updating SPG in eclipse/scanning. Please wait..." &&
 echo "" &&
 
 git clone git@github.com:dls-controls/scanpointgenerator.git /tmp/scanpointgenerator &&
-git clone git@github.com:DiamondLightSource/daq-eclipse.git /tmp/daq-eclipse &&
-cd /tmp/daq-eclipse &&
+git clone git@github.com:eclipse/scanning.git /tmp/scanning &&
+cd /tmp/scanning &&
 git checkout -b spg-release-$VER &&
 git rm -r org.eclipse.scanning.points/scripts/scanpointgenerator &&
 cp -r ../scanpointgenerator/scanpointgenerator/ org.eclipse.scanning.points/scripts/scanpointgenerator &&
 git add -A &&
 git commit -m "Update scanpointgenerator to version $VER" &&
-git push origin spg-release-$VER
+git push origin spg-release-$VER &&
 
 echo "" &&
 echo "Update successful"
 
 rm -rf /tmp/scanpointgenerator
-rm -rf /tmp/daq-eclipse
+rm -rf /tmp/scanning
