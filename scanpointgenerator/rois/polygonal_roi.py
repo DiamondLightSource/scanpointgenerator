b575a52fb0f1e1467a39f19f996d378a2320c752
Charles Mita <charles.mita@diamond.ac.uk>
2017-03-10 18:09:30 +0000

    Change PolygonalROI to take x, y arrays separately

--- scanpointgenerator/rois/polygonal_roi.py
+++ scanpointgenerator/rois/polygonal_roi.py
@@ -5,54 +5,60 @@ from scanpointgenerator.compat import range_, np
 @ROI.register_subclass("scanpointgenerator:roi/PolygonalROI:1.0")
 class PolygonalROI(ROI):
 
-    def __init__(self, points):
+    def __init__(self, points_x, points_y):
+        """
+        Args:
+            points_x (list(double)): x positions for polygon vertices
+            points_y (list(double)): y positions for polygon vertices
+        """
+
         super(PolygonalROI, self).__init__()
-        if len(points) < 3:
+        if len(points_x) != len(points_y):
+            raise ValueError("Point arrays must be the same size")
+        if len(points_x) < 3:
             raise ValueError("Polygon requires at least 3 vertices")
         # TODO: check points are not all collinear
         #       (i.e. describe at least a triangle)
-        self.points = points
+        self.points_x = points_x
+        self.points_y = points_y
 
     def contains_point(self, point):
         # Uses ray-casting algorithm - "fails" for complex (self-intersecting)
         # polygons, but this is intended behaviour for the moment
         # Haines 1994
         inside = False
-        n = len(self.points)
         x = point[0]
         y = point[1]
-        v1 = self.points[-1]
-        for i in range(0, n):
-            v2 = self.points[i]
-            if (v1[1] <= y and v2[1] > y) or (v1[1] > y and v2[1] <= y):
-                t = float(y - v1[1]) / float(v2[1] - v1[1])
-                if x < v1[0] + t * (v2[0] - v1[0]):
+        v1x, v1y = self.points_x[-1], self.points_y[-1]
+        for v2x, v2y in zip(self.points_x, self.points_y):
+            if (v1y <= y and v2y > y) or (v1y > y and v2y <= y):
+                t = float(y - v1y) / float(v2y - v1y)
+                if x < v1x + t * (v2x - v1x):
                     inside = not inside
-            v1 = v2
+            v1x, v1y = v2x, v2y
         return inside
 
     def mask_points(self, points):
         x = points[0]
         y = points[1]
-        v1 = self.points[-1]
+        v1x, v1y = self.points_x[-1], self.points_y[-1]
         mask = np.full(len(x), False, dtype=bool)
-        for i in range_(0, len(self.points)):
-            v2 = self.points[i]
-            if (v2[1] == v1[1]):
-                # skip horizontal edges
-                v1 = v2
-                continue
-            vmask = ((y < v2[1]) & (y >= v1[1])) | ((y < v1[1]) & (y >= v2[1]))
-            t = (y - v2[1]) / (v2[1] - v1[1])
-            vmask &= x < v1[0] + t * (v2[0] - v1[0])
-            mask ^= vmask
-            v1 = v2
+        for v2x, v2y in zip(self.points_x, self.points_y):
+            # skip horizontal edges
+            if (v2y != v1y):
+                vmask = ((y < v2y) & (y >= v1y)) | ((y < v1y) & (y >= v2y))
+                t = (y - v2y) / (v2y - v1y)
+                vmask &= x < v1x + t * (v2x - v1x)
+                mask ^= vmask
+            v1x, v1y = v2x, v2y
         return mask
 
     def to_dict(self):
         d = super(PolygonalROI, self).to_dict()
-        d["points"] = self.points
+        d["points_x"] = self.points_x
+        d["points_y"] = self.points_y
         return d
+
     @classmethod
     def from_dict(cls, d):
-        return cls(d["points"])
+        return cls(d["points_x"], d["points_y"])
