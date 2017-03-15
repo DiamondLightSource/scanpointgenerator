906d938725ea45386565b8e44eef4193083d95a3
Charles Mita <charles.mita@diamond.ac.uk>
2017-03-15 13:52:05 +0000

    Use assertListAlmostEquals for Spiral/Lissajous position tests
    Jython will not produce the exact same floating point values as
    CPython/numpy and we need to account for that in our unit tests.

--- tests/test_generators/test_spiralgenerator.py
+++ tests/test_generators/test_spiralgenerator.py
@@ -7,7 +7,7 @@ from test_util import ScanPointGeneratorTest
 from scanpointgenerator import SpiralGenerator
 
 
-class SpiralGeneratorTest(unittest.TestCase):
+class SpiralGeneratorTest(ScanPointGeneratorTest):
 
     def setUp(self):
         self.g = SpiralGenerator(['x', 'y'], ["cm", "mm"], [0.0, 0.0], 1.4, alternate=True)
@@ -21,27 +21,24 @@ class SpiralGeneratorTest(unittest.TestCase):
             SpiralGenerator(["x", "x"], ["mm", "mm"], [0.0, 0.0], 1.0)
 
     def test_array_positions(self):
-        positions = [{'y': -0.3211855677650875, 'x': 0.23663214944574582},
-                     {'y': -0.25037538922751695, 'x': -0.6440318266552169},
-                     {'y': 0.6946549630820702, 'x': -0.5596688286164636},
-                     {'y': 0.9919687803189761, 'x': 0.36066957248394327},
-                     {'y': 0.3924587351155914, 'x': 1.130650533568409},
-                     {'y': -0.5868891557832875, 'x': 1.18586065489788},
-                     {'y': -1.332029488076613, 'x': 0.5428735608675326}]
-        bounds = [{'y':0.0, 'x':0.0},
-                 {'y': -0.5189218293602549, 'x': -0.2214272368007088},
-                 {'y': 0.23645222432582483, 'x': -0.7620433832656455},
-                 {'y': 0.9671992383675001, 'x': -0.13948222773063082},
-                 {'y': 0.7807653675717078, 'x': 0.8146440851904461},
-                 {'y': -0.09160107657707395, 'x': 1.2582363345925418},
-                 {'y': -1.0190886264001306, 'x': 0.9334439933089926},
-                 {'y': -1.4911377166541206, 'x': 0.06839234794968006}]
+        expected_x = [0.23663214944574582, -0.6440318266552169,
+            -0.5596688286164636, 0.36066957248394327, 1.130650533568409,
+            1.18586065489788, 0.5428735608675326]
+        expected_y = [-0.3211855677650875, -0.25037538922751695,
+            0.6946549630820702, 0.9919687803189761, 0.3924587351155914,
+            -0.5868891557832875, -1.332029488076613,]
+        expected_bx = [0.0, -0.2214272368007088, -0.7620433832656455,
+            -0.13948222773063082, 0.8146440851904461, 1.2582363345925418,
+            0.9334439933089926, 0.06839234794968006]
+        expected_by = [0.0, -0.5189218293602549, 0.23645222432582483,
+            0.9671992383675001, 0.7807653675717078, -0.09160107657707395,
+            -1.0190886264001306, -1.4911377166541206]
         self.g.prepare_positions()
         self.g.prepare_bounds()
-        p = [{"x":x, "y":y} for (x, y) in zip(self.g.positions['x'], self.g.positions['y'])]
-        b = [{"x":x, "y":y} for (x, y) in zip(self.g.bounds['x'], self.g.bounds['y'])]
-        self.assertEqual(positions, p)
-        self.assertEqual(bounds, b)
+        self.assertListAlmostEqual(expected_x, self.g.positions['x'].tolist())
+        self.assertListAlmostEqual(expected_y, self.g.positions['y'].tolist())
+        self.assertListAlmostEqual(expected_bx, self.g.bounds['x'].tolist())
+        self.assertListAlmostEqual(expected_by, self.g.bounds['y'].tolist())
 
     def test_to_dict(self):
         expected_dict = dict()
