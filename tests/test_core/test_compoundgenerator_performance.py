502d0a6f3740acb2ed87ad09002c2ec282621914
Charles Mita <charles.mita@diamond.ac.uk>
2017-03-15 13:08:11 +0000

    Turn down compound generator performance testing for Jython
    Travis VMs are not very powerful and only allow 3Gb of memory - the
    performance test can easily blow past that when using Jython (due to
    Java overheads).

--- tests/test_core/test_compoundgenerator_performance.py
+++ tests/test_core/test_compoundgenerator_performance.py
@@ -12,13 +12,20 @@ from scanpointgenerator import ROIExcluder
 from scanpointgenerator.rois import CircularROI
 from scanpointgenerator.mutators import RandomOffsetMutator
 
+# Test 20 million points on Jython (Travis runs out of memory at 200 million)
+ZSIZE = 10 if os.name == "java" else 100
+# Normally this should take CPython ~5 seconds on a reasonable machine for
+# for 200 million points, but Travis VMs are sometimes quite weak.
+# Jython gets less time because it's doing fewer points (but around 3 times slower)
+TIMELIMIT = 5 if os.name == "java" else 12
+
 class CompoundGeneratorPerformanceTest(ScanPointGeneratorTest):
     def test_200_million_time_constraint(self):
         start_time = time.time()
 
         s = SpiralGenerator(
             ["x", "y"], "mm", [0, 0], 6, 0.02, True) # ~2e5 points
-        z = LineGenerator("z", "mm", 0, 1, 100, True) #1e2 points
+        z = LineGenerator("z", "mm", 0, 1, ZSIZE, True) #1e2 points or 1e1 for Jython
         w = LineGenerator("w", "mm", 0, 1, 10, True) #1e1 points
         r1 = CircularROI([-0.7, 4], 0.5)
         r2 = CircularROI([0.5, 0.5], 0.3)
@@ -31,10 +38,8 @@ class CompoundGeneratorPerformanceTest(ScanPointGeneratorTest):
         g.prepare() # g.size ~3e5
 
         end_time = time.time()
-        #self.assertLess(end_time - start_time, 5)
-        # TravisCI VMs are sometimes pretty weak
         # if this test becomes problematic then we'll just have to remove it
-        self.assertLess(end_time - start_time, 12)
+        self.assertLess(end_time - start_time, TIMELIMIT)
 
         # we dont care about this right now
         #start_time = time.time()
