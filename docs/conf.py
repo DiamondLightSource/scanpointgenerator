a0ff32394af615eaa69d4274493bec4358f8921c
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-03-21 14:33:43 +0000

    Fix docs build for dls only

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ except:
 else:
     require("matplotlib")
     require("scipy")
+    require("annotypes")
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..')))
 
 # -- General configuration ------------------------------------------------
